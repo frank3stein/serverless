fc0bfd091d621f6ff6b3016b1747fdc9b7f6762e
Emil Martinov <martinov.emil@gmail.com>
2020-01-20 23:55:16 +0100

    Prettier added, default settings. CreateTodoModel is fixed. There were unneccessary required fields

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,28 +1,34 @@
-import 'source-map-support/register'
+import "source-map-support/register";
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from "aws-lambda";
 
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-import { getUserId } from '../utils';
-import {updateTodo} from '../businessLogic/logic';
+import { UpdateTodoRequest } from "../../requests/UpdateTodoRequest";
+import { getUserId } from "../utils";
+import { updateTodo } from "../businessLogic/logic";
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   //@ts-ignore -- source will be added by the warmup plugin
-  if(event.source === 'serverless-plugin-warmup'){
+  if (event.source === "serverless-plugin-warmup") {
     //@ts-ignore
-    return 'This function is hot !'
+    return "This function is hot !";
   }
-  const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  const todoId = event.pathParameters.todoId;
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
   const userId = getUserId(event);
 
-  await updateTodo({userId, todoId, updatedTodo});
+  await updateTodo({ userId, todoId, updatedTodo });
   return {
     statusCode: 200,
-    headers:{
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
     },
-    body: ''
-  }
-}
+    body: ""
+  };
+};
